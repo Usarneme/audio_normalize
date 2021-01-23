c64ae0dbb8c3db0a462074f7d6d081f15eb9b0dd
Usarneme <jamestcoop@gmail.com>
2021-01-23 10:23:44 -0800

    Started writing out the script - checking for command line args and exiting/giving useful error info; looking for a provided directory and reading all files in it; included progress bar which updates as files are ready; added color package to allow colorful messages on the cli interface.

--- audio_normalize.go
+++ audio_normalize.go
@@ -2,8 +2,63 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/schollz/progressbar/v3"
 )
 
 func main() {
-	fmt.Printf("Starting audio_normalize...")
+	fmt.Print("\n")
+	color.Red(`/\ |_| |) | ()   |\| () /? |\/| /\ |_ | ~/_ [-`)
+	fmt.Print("\n")
+
+	if len(os.Args) < 2 {
+		fmt.Println("[usage error] - please provide a directory in which to look for video files.")
+		fmt.Println("Try again, such as: ~ ./audio_normalize movies/")
+		os.Exit(-1)
+	}
+
+	dir := os.Args[1]
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(files) > 1 {
+		fmt.Printf("Found %d files in dir '%s/'\n", len(files), dir)
+	} else {
+		fmt.Printf("Found %d file in dir '%s/'\n", len(files), dir)
+	}
+
+	osd := fmt.Sprintf(" * Reading %d files...", len(files))
+
+	bar := progressbar.NewOptions(len(files),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowBytes(true),
+		// progressbar.OptionSetWidth(15),
+		progressbar.OptionSetDescription(osd),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[green]=[reset]",
+			SaucerHead:    "[green]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}))
+
+	names := make([]string, len(files))
+
+	for i := 0; i < len(files); i++ {
+		// fmt.Printf("\t\tFound video file '%s'", files[i].Name())
+		bar.Add(1)
+		names = append(names, files[i].Name())
+		time.Sleep(140 * time.Millisecond)
+	}
+
+	fmt.Printf("\n * All files: %v\n", names)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module https://github.com/Usarneme/audio_normalize
+module https: //github.com/Usarneme/audio_normalize
 
 go 1.15
+
+require (
+	github.com/fatih/color v1.10.0
+	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/schollz/progressbar/v3 v3.7.3
+	golang.org/x/sys v0.0.0-20210123111255-9b0068b26619 // indirect
+)
