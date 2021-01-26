6bf541306a628ef28885a108cbe59fbce3785062
Usarneme <jamestcoop@gmail.com>
2021-01-25 23:27:08 -0800

    Add vscode liveshare restriction setting. Add all formats that ffmpeg can handle to filter filetypes in the provided directory. Add contains function to see if a string provided is contained within the ffmpeg formats array.

--- audio_normalize.go
+++ audio_normalize.go
@@ -6,9 +6,21 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 )
+
+var formats = [...]string{"3g2", "3gp", "4xm", "a64", "aa", "aac", "ac3", "acm", "act", "adf", "adp", "ads", "adts", "adx", "aea", "afc", "aiff", "aix", "alaw", "alias_pix", "alp", "amr", "amrnb", "amrwb", "anm", "apc", "ape", "apm", "apng", "aptx", "aptx_hd", "aqtitle", "argo_asf", "asf", "asf_o", "asf_stream", "ass", "ast", "au", "av1", "avfoundation", "avi", "avm2", "avr", "avs", "avs2", "bethsoftvid", "bfi", "bfstm", "bin", "bink", "bit", "bmp_pipe", "bmv", "boa", "brender_pix", "brstm", "c93", "caf", "cavsvideo", "cdg", "cdxl", "cine", "codec2", "codec2raw", "concat", "crc", "dash", "data", "daud", "dcstr", "dds_pipe", "derf", "dfa", "dhav", "dirac", "dnxhd", "dpx_pipe", "dsf", "dsicin", "dss", "dts", "dtshd", "dv", "dvbsub", "dvbtxt", "dvd", "dxa", "ea", "ea_cdata", "eac3", "epaf", "exr_pipe", "f32be", "f32le", "f4v", "f64be", "f64le", "ffmetadata", "fifo", "fifo_test", "film_cpk", "filmstrip", "fits", "flac", "flic", "flv", "framecrc", "framehash", "framemd5", "frm", "fsb", "fwse", "g722", "g723_1", "g726", "g726le", "g729", "gdv", "genh", "gif", "gif_pipe", "gsm", "gxf", "h261", "h263", "h264", "hash", "hca", "hcom", "hds", "hevc", "hls", "hnm", "ico", "idcin", "idf", "iff", "ifv", "ilbc", "image2", "image2pipe", "ingenient", "ipmovie", "ipod", "ircam", "ismv", "iss", "iv8", "ivf", "ivr", "j2k_pipe", "jacosub", "jpeg_pipe", "jpegls_pipe", "jv", "kux", "kvag", "latm", "lavfi", "live_flv", "lmlm4", "loas", "lrc", "lvf", "lxf", "m4v", "matroska", "md5", "mgsts", "microdvd", "mjpeg", "mjpeg_2000", "mkvtimestamp_v2", "mlp", "mlv", "mm", "mmf", "mov", "mp4", "m4a", "3gp", "3g2", "mj2", "mp2", "mp3", "mp4", "mpc", "mpc8", "mpeg", "mpeg1video", "mpeg2video", "mpegts", "mpegtsraw", "mpegvideo", "mpjpeg", "mpl2", "mpsub", "msf", "msnwctcp", "mtaf", "mtv", "mulaw", "musx", "mv", "mvi", "mxf", "mxf_d10", "mxf_opatom", "mxg", "nc", "nistsphere", "nsp", "nsv", "null", "nut", "nuv", "oga", "ogg", "ogv", "oma", "opus", "paf", "pam_pipe", "pbm_pipe", "pcx_pipe", "pgm_pipe", "pgmyuv_pipe", "pictor_pipe", "pjs", "pmp", "png_pipe", "pp_bnk", "ppm_pipe", "psd_pipe", "psp", "psxstr", "pva", "pvf", "qcp", "qdraw_pipe", "r3d", "rawvideo", "realtext", "redspark", "rl2", "rm", "roq", "rpl", "rsd", "rso", "rtp", "rtp_mpegts", "rtsp", "s16be", "s16le", "s24be", "s24le", "s32be", "s32le", "s337m", "s8", "sami", "sap", "sbc", "sbg", "scc", "sdl", "sdp", "sdr2", "sds", "sdx", "segment", "ser", "sgi_pipe", "shn", "siff", "singlejpeg", "sln", "smjpeg", "smk", "smoothstreaming", "smush", "sol", "sox", "spdif", "spx", "srt", "stl", "stream_segment", "streamhash", "subviewer", "subviewer1", "sunrast_pipe", "sup", "svag", "svcd", "svg_pipe", "swf", "tak", "tedcaptions", "tee", "thp", "tiertexseq", "tiff_pipe", "tmv", "truehd", "tta", "tty", "txd", "ty", "u16be", "u16le", "u24be", "u24le", "u32be", "u32le", "u8", "uncodedframecrc", "v210", "v210x", "vag", "vc1", "vc1test", "vcd", "vidc", "vividas", "vivo", "vmd", "vob", "vobsub", "voc", "vpk", "vplayer", "vqf", "w64", "wav", "wc3movie", "webm", "webm_chunk", "webm_dash_manifest", "webp", "webp_pipe", "webvtt", "wsaud", "wsd", "wsvqa", "wtv", "wv", "wve", "xa", "xbin", "xmv", "xpm_pipe", "xvag", "xwd_pipe", "xwma", "yop", "yuv4mpegpipe"}
+
+func contains(arr [len(formats)]string, query string) bool {
+	for _, val := range arr {
+		if val == query {
+			return true
+		}
+	}
+	return false
+}
 
 func collectFilenames(dir string) ([]os.FileInfo, error) {
 	color.Cyan("Reading all files in the directory '%s/'", dir)
@@ -44,33 +56,12 @@ func main() {
 		os.Exit(-1)
 	}
 
-	// create a slice to hold all of the subdirectory/filenames of files to be audio normalized
 	filepaths := []string{}
-
 	dir := os.Args[1]
 	files, readErr := collectFilenames(dir)
 	if readErr != nil {
 		color.Red("[ERROR]: %s", readErr)
 	}
-	// color.Green("After collecting file names:")
-	// for _, v := range files {
-	// 	fmt.Printf("\n\t%+v\n", v)
-	// }
-
-	// example result of printf for a file:
-	// &{name:big_buck_bunny_480p_stereo.avi
-	// size:156506028
-	// mode:420
-	// modTime:{wall:26551519 ext:63747019898 loc:0x117d380}
-	// sys:{Dev:16777220 Mode:33188 Nlink:1 Ino:3075165 Uid:501 Gid:20 Rdev:0 Pad_cgo_0:[0 0 0 0] Atimespec:{Sec:1611537984 Nsec:934362000} Mtimespec:{Sec:1611423098 Nsec:26551519} Ctimespec:{Sec:1611538228 Nsec:609836550} Birthtimespec:{Sec:1611423097 Nsec:926494001}
-	// Size:156506028 Blocks:305680 Blksize:4096 Flags:0 Gen:0 Lspare:0 Qspare:[0 0]}}
-
-	// example printf of a directory:
-	// &{name:tmp size:64
-	// mode:2147484141
-	// modTime:{wall:362958889 ext:63747139758 loc:0x117d380}
-	// sys:{Dev:16777220 Mode:16877 Nlink:2 Ino:3125540 Uid:501 Gid:20 Rdev:0 Pad_cgo_0:[0 0 0 0] Atimespec:{Sec:1611542958 Nsec:362958889} Mtimespec:{Sec:1611542958 Nsec:362958889} Ctimespec:{Sec:1611542958 Nsec:362958889} Birthtimespec:{Sec:1611542958 Nsec:362958889}
-	// Size:64 Blocks:0 Blksize:4096 Flags:0 Gen:0 Lspare:0 Qspare:[0 0]}}
 
 	fmt.Println("\nLooping through returned os.FileInfo slice for filenames...")
 	for i := 0; i < len(files); i++ {
@@ -79,8 +70,12 @@ func main() {
 			color.Magenta("Folder found! %s", files[i].Name())
 			// TODO => files, readErr := collectFilenames(files[i].Name())
 		} else {
-			// TODO verify filetype is avi, mov, etc...
-			filepaths = append(filepaths, files[i].Name())
+			name := strings.Split(files[i].Name(), ".")[0]
+			ext := strings.Split(files[i].Name(), ".")[1]
+			fmt.Printf("Checking filetype of %s.%s\n", name, ext)
+			if contains(formats, ext) {
+				filepaths = append(filepaths, files[i].Name())
+			}
 		}
 	}
 	// interface for os.FileInfo as returned by ioutil.ReadDir(), files is a slice of these:
@@ -95,11 +90,13 @@ func main() {
 
 	color.Green("After looping, created filepaths slice of: %s", filepaths)
 
+	// ~ ffmpeg -i big_buck_bunny_480p_stereo.avi -filter:a loudnorm -c:v copy NA_big_buck_bunny.avi
+
 	// cmd := fmt.Sprintf("-i files/big_buck_bunny_480p_stereo.avi -filter:a loudnorm files/big_buck_bunny_480p_stereo_NA.avi", path, path)
 	// fmt.Printf("\nPreparing to execute command: 'ffmpeg %s'", cmd)
 
 	// working code for executing a single ffmpeg routine
-	// cmd := exec.Command("ffmpeg", "-i", "files/big_buck_bunny_480p_stereo.avi", "-filter:a", "loudnorm", "files/big_buck_bunny_480p_stereo_NA.avi")
+	// cmd := exec.Command("ffmpeg", "-i", "files/big_buck_bunny_480p_stereo.avi", "-filter:a", "loudnorm", "-c:v", "copy", "files/big_buck_bunny_480p_stereo_NA.avi")
 	// cmd.Stdin = os.Stdin
 	// cmd.Stdout = os.Stdout
 	// cmd.Stderr = os.Stderr
