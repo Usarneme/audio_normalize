44ecdb52872d5b6e5fe2b32693352ad1ebf97664
Usarneme <jamestcoop@gmail.com>
2021-01-29 16:06:37 -0800

    Remove extra comments/error logging. Simplify console logging and error reporting to a log file created in output. Make program system-agnostic by adding FileSeparator from the OS package instead of hard-coded / unix-style dir/file strings. Started fleshing out CLI flags such as prepend/append output filename optional tags, etc.

--- audio_normalize.go
+++ audio_normalize.go
@@ -14,10 +14,11 @@ import (
 	"github.com/fatih/color"
 )
 
-var formats = [...]string{"3g2", "3gp", "4xm", "a64", "aa", "aac", "ac3", "acm", "act", "adf", "adp", "ads", "adts", "adx", "aea", "afc", "aiff", "aix", "alaw", "alias_pix", "alp", "amr", "amrnb", "amrwb", "anm", "apc", "ape", "apm", "apng", "aptx", "aptx_hd", "aqtitle", "argo_asf", "asf", "asf_o", "asf_stream", "ass", "ast", "au", "av1", "avfoundation", "avi", "avm2", "avr", "avs", "avs2", "bethsoftvid", "bfi", "bfstm", "bin", "bink", "bit", "bmp_pipe", "bmv", "boa", "brender_pix", "brstm", "c93", "caf", "cavsvideo", "cdg", "cdxl", "cine", "codec2", "codec2raw", "concat", "crc", "dash", "data", "daud", "dcstr", "dds_pipe", "derf", "dfa", "dhav", "dirac", "dnxhd", "dpx_pipe", "dsf", "dsicin", "dss", "dts", "dtshd", "dv", "dvbsub", "dvbtxt", "dvd", "dxa", "ea", "ea_cdata", "eac3", "epaf", "exr_pipe", "f32be", "f32le", "f4v", "f64be", "f64le", "ffmetadata", "fifo", "fifo_test", "film_cpk", "filmstrip", "fits", "flac", "flic", "flv", "framecrc", "framehash", "framemd5", "frm", "fsb", "fwse", "g722", "g723_1", "g726", "g726le", "g729", "gdv", "genh", "gif", "gif_pipe", "gsm", "gxf", "h261", "h263", "h264", "hash", "hca", "hcom", "hds", "hevc", "hls", "hnm", "ico", "idcin", "idf", "iff", "ifv", "ilbc", "image2", "image2pipe", "ingenient", "ipmovie", "ipod", "ircam", "ismv", "iss", "iv8", "ivf", "ivr", "j2k_pipe", "jacosub", "jpeg_pipe", "jpegls_pipe", "jv", "kux", "kvag", "latm", "lavfi", "live_flv", "lmlm4", "loas", "lrc", "lvf", "lxf", "m4v", "matroska", "md5", "mgsts", "microdvd", "mjpeg", "mjpeg_2000", "mkvtimestamp_v2", "mlp", "mlv", "mm", "mmf", "mov", "mp4", "m4a", "3gp", "3g2", "mj2", "mp2", "mp3", "mp4", "mpc", "mpc8", "mpeg", "mpeg1video", "mpeg2video", "mpegts", "mpegtsraw", "mpegvideo", "mpjpeg", "mpl2", "mpsub", "msf", "msnwctcp", "mtaf", "mtv", "mulaw", "musx", "mv", "mvi", "mxf", "mxf_d10", "mxf_opatom", "mxg", "nc", "nistsphere", "nsp", "nsv", "null", "nut", "nuv", "oga", "ogg", "ogv", "oma", "opus", "paf", "pam_pipe", "pbm_pipe", "pcx_pipe", "pgm_pipe", "pgmyuv_pipe", "pictor_pipe", "pjs", "pmp", "png_pipe", "pp_bnk", "ppm_pipe", "psd_pipe", "psp", "psxstr", "pva", "pvf", "qcp", "qdraw_pipe", "r3d", "rawvideo", "realtext", "redspark", "rl2", "rm", "roq", "rpl", "rsd", "rso", "rtp", "rtp_mpegts", "rtsp", "s16be", "s16le", "s24be", "s24le", "s32be", "s32le", "s337m", "s8", "sami", "sap", "sbc", "sbg", "scc", "sdl", "sdp", "sdr2", "sds", "sdx", "segment", "ser", "sgi_pipe", "shn", "siff", "singlejpeg", "sln", "smjpeg", "smk", "smoothstreaming", "smush", "sol", "sox", "spdif", "spx", "srt", "stl", "stream_segment", "streamhash", "subviewer", "subviewer1", "sunrast_pipe", "sup", "svag", "svcd", "svg_pipe", "swf", "tak", "tedcaptions", "tee", "thp", "tiertexseq", "tiff_pipe", "tmv", "truehd", "tta", "tty", "txd", "ty", "u16be", "u16le", "u24be", "u24le", "u32be", "u32le", "u8", "uncodedframecrc", "v210", "v210x", "vag", "vc1", "vc1test", "vcd", "vidc", "vividas", "vivo", "vmd", "vob", "vobsub", "voc", "vpk", "vplayer", "vqf", "w64", "wav", "wc3movie", "webm", "webm_chunk", "webm_dash_manifest", "webp", "webp_pipe", "webvtt", "wsaud", "wsd", "wsvqa", "wtv", "wv", "wve", "xa", "xbin", "xmv", "xpm_pipe", "xvag", "xwd_pipe", "xwma", "yop", "yuv4mpegpipe"}
+// var formats = [...]string{"3g2", "3gp", "4xm", "a64", "aa", "aac", "ac3", "acm", "act", "adf", "adp", "ads", "adts", "adx", "aea", "afc", "aiff", "aix", "alaw", "alias_pix", "alp", "amr", "amrnb", "amrwb", "anm", "apc", "ape", "apm", "apng", "aptx", "aptx_hd", "aqtitle", "argo_asf", "asf", "asf_o", "asf_stream", "ass", "ast", "au", "av1", "avfoundation", "avi", "avm2", "avr", "avs", "avs2", "bethsoftvid", "bfi", "bfstm", "bin", "bink", "bit", "bmp_pipe", "bmv", "boa", "brender_pix", "brstm", "c93", "caf", "cavsvideo", "cdg", "cdxl", "cine", "codec2", "codec2raw", "concat", "crc", "dash", "data", "daud", "dcstr", "dds_pipe", "derf", "dfa", "dhav", "dirac", "dnxhd", "dpx_pipe", "dsf", "dsicin", "dss", "dts", "dtshd", "dv", "dvbsub", "dvbtxt", "dvd", "dxa", "ea", "ea_cdata", "eac3", "epaf", "exr_pipe", "f32be", "f32le", "f4v", "f64be", "f64le", "ffmetadata", "fifo", "fifo_test", "film_cpk", "filmstrip", "fits", "flac", "flic", "flv", "framecrc", "framehash", "framemd5", "frm", "fsb", "fwse", "g722", "g723_1", "g726", "g726le", "g729", "gdv", "genh", "gif", "gif_pipe", "gsm", "gxf", "h261", "h263", "h264", "hash", "hca", "hcom", "hds", "hevc", "hls", "hnm", "ico", "idcin", "idf", "iff", "ifv", "ilbc", "image2", "image2pipe", "ingenient", "ipmovie", "ipod", "ircam", "ismv", "iss", "iv8", "ivf", "ivr", "j2k_pipe", "jacosub", "jpeg_pipe", "jpegls_pipe", "jv", "kux", "kvag", "latm", "lavfi", "live_flv", "lmlm4", "loas", "lrc", "lvf", "lxf", "m4v", "matroska", "md5", "mgsts", "microdvd", "mjpeg", "mjpeg_2000", "mkv", "mkvtimestamp_v2", "mlp", "mlv", "mm", "mmf", "mov", "mp4", "m4a", "3gp", "3g2", "mj2", "mp2", "mp4", "mpc", "mpc8", "mpeg", "mpeg1video", "mpeg2video", "mpegts", "mpegtsraw", "mpegvideo", "mpjpeg", "mpl2", "mpsub", "msf", "msnwctcp", "mtaf", "mtv", "mulaw", "musx", "mv", "mvi", "mxf", "mxf_d10", "mxf_opatom", "mxg", "nc", "nistsphere", "nsp", "nsv", "null", "nut", "nuv", "oga", "ogg", "ogv", "oma", "opus", "paf", "pam_pipe", "pbm_pipe", "pcx_pipe", "pgm_pipe", "pgmyuv_pipe", "pictor_pipe", "pjs", "pmp", "png_pipe", "pp_bnk", "ppm_pipe", "psd_pipe", "psp", "psxstr", "pva", "pvf", "qcp", "qdraw_pipe", "r3d", "rawvideo", "realtext", "redspark", "rl2", "rm", "roq", "rpl", "rsd", "rso", "rtp", "rtp_mpegts", "rtsp", "s16be", "s16le", "s24be", "s24le", "s32be", "s32le", "s337m", "s8", "sami", "sap", "sbc", "sbg", "scc", "sdl", "sdp", "sdr2", "sds", "sdx", "segment", "ser", "sgi_pipe", "shn", "siff", "singlejpeg", "sln", "smjpeg", "smk", "smoothstreaming", "smush", "sol", "sox", "spdif", "spx", "srt", "stl", "stream_segment", "streamhash", "subviewer", "subviewer1", "sunrast_pipe", "sup", "svag", "svcd", "svg_pipe", "swf", "tak", "tedcaptions", "tee", "thp", "tiertexseq", "tiff_pipe", "tmv", "truehd", "tta", "tty", "txd", "ty", "u16be", "u16le", "u24be", "u24le", "u32be", "u32le", "u8", "uncodedframecrc", "v210", "v210x", "vag", "vc1", "vc1test", "vcd", "vidc", "vividas", "vivo", "vmd", "vob", "vobsub", "voc", "vpk", "vplayer", "vqf", "w64", "wc3movie", "webm", "webm_chunk", "webm_dash_manifest", "webp", "webp_pipe", "webvtt", "wsaud", "wsd", "wsvqa", "wtv", "wv", "wve", "xa", "xbin", "xmv", "xpm_pipe", "xvag", "xwd_pipe", "xwma", "yop", "yuv4mpegpipe"}
+var formats = [...]string{"avi", "mkv", "mp4"}
 
 func collectFilenames(dir string) ([]string, error) {
-	color.Cyan("[func] Collecting all filenames within '%s/'", dir)
+	color.Cyan("[func] Collecting all filenames within '%s%s'", dir, string(os.PathSeparator))
 
 	var files []string
 	err := filepath.Walk(dir,
@@ -26,35 +27,31 @@ func collectFilenames(dir string) ([]string, error) {
 				return err
 			}
 			length := len(info.Name())
-			idx := 0
 			var shortName string
 			if length > 20 {
-				idx = length % 20
-				shortName = "..." + info.Name()[idx:]
+				shortName = "..." + info.Name()[length%20:]
 			} else {
-				shortName = info.Name()[idx:]
+				shortName = info.Name()
 			}
 
 			if info.IsDir() == true {
-				color.Blue("\tSKIPPED \t folder\t '%s'", shortName)
+				color.Blue("\tSKIPPED \tDirectory\t'%s'", shortName)
 			} else {
 				lastIndex := strings.LastIndex(info.Name(), ".")
 				ext := info.Name()[lastIndex+1:]
 				if contains(formats, ext) {
-					color.Magenta("\tAPPENDED\t a/v file\t '%s'", shortName)
+					color.Magenta("\tAPPENDED\tAud/Vis File\t'%s'", shortName)
 					files = append(files, path)
 				} else {
-					color.Blue("\tSKIPPED \t non-a/v file\t '%s'", shortName)
+					color.Blue("\tSKIPPED \tUnsupported\t'%s'", shortName)
 				}
 			}
 			return nil
 		})
-	// after walking all directories and subdirectories...
+
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
-	color.Green("[func] Returning from collectFilenames - %v", files)
 	return files, nil
 }
 
@@ -62,19 +59,14 @@ func doNormalization(wg *sync.WaitGroup, file string) {
 	defer wg.Done()
 	color.Cyan("[func] Preparing to normalize file at '%s", file)
 
-	lastIndex := strings.LastIndex(file, "/")
+	lastIndex := strings.LastIndex(file, string(os.PathSeparator))
 	name := file[lastIndex+1:]
-	outName := fmt.Sprintf("output/%s", name)
-
-	// command with stderr passed to a temp file descriptor
-	// cmd := exec.Command("exec", "3>&1;")
-	//  "sout=$(ffmpeg -i files/tmp/some.avi -filter:a loudnorm -c:v copy NA_big_buck_bunny.avi -loglevel error 2>&1 1>&3); exec 3>&-; echo $sout;")
-
+	prepend := "NA" // placeholder for cli flag
+	separator := "_"
+	outName := fmt.Sprintf("output%c%s%s%s", os.PathSeparator, prepend, separator, name)
 	cmd := exec.Command("ffmpeg", "-loglevel", "error", "-i", file, "-filter:a", "loudnorm", "-c:v", "copy", outName)
 
-	// duplicate output files are ignored/not overwritten by default, so we don't need any interactivity via stdin
-	// '-loglevel error' suppresses stdout logging of successful processing
-	// stderr output is piped so we can use it for logging results/informing the user
+	// stderr output is piped so we can use it for logging results
 	stderr, _ := cmd.StderrPipe()
 	if err := cmd.Start(); err != nil {
 		color.Red("  [ERROR] %v", err)
@@ -92,9 +84,10 @@ func doNormalization(wg *sync.WaitGroup, file string) {
 
 func updateLog(msg string) {
 	color.Cyan("[func] Outputting error msg to file: '%s'", msg)
-	if fileExists("output/log.txt") == true {
+	path := fmt.Sprintf("output%slog.txt", string(os.PathSeparator))
+	if fileExists(path) == true {
 		// already exists, append to file
-		f, err := os.OpenFile("output/log.txt", os.O_APPEND|os.O_WRONLY, 0644)
+		f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0644)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -111,7 +104,7 @@ func updateLog(msg string) {
 		}
 	} else {
 		// create the log file
-		f, err := os.Create("output/log.txt")
+		f, err := os.Create(path)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -145,14 +138,15 @@ func main() {
 
 	if len(os.Args) < 2 {
 		fmt.Println("[usage error] - please provide a directory in which to look for video files.")
-		fmt.Println("Try again, such as: ~ ./audio_normalize movies/")
-		os.Exit(-1)
+		fmt.Println("Try again, such as: \nLinux/Mac: \t~ ./audio_normalize movies/ \nWindows\t audio_normalize.exe movies\\")
+		os.Exit(1)
 	}
 
 	dir := os.Args[1]
 	files, readErr := collectFilenames(dir)
 	if readErr != nil {
 		color.Red("[ERROR]: %s", readErr)
+		os.Exit(1)
 	}
 
 	// worker waitgroup to ensure all goroutines are completed before exiting program
@@ -165,6 +159,6 @@ func main() {
 	}
 
 	wg.Wait()
-	color.Red("[main] Workers completed. Exiting.")
-	color.Yellow("[main] Please check output/log.txt for any error details.")
+	color.Red("[main] All workers finished and audio normalization complete!")
+	color.Yellow("[main] Please check the file log.txt in the output/ directory for any error details.")
 }
